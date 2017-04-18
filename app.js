3fd31dc0d62cfff0e4a549a7557aa36f8f9adb6e
krishnapriya <krishnapriyakamesh@gmail.com>
2017-04-18 11:18:43 -0500

    working on testing

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ var routes = require('./routes/');
 
 var app = express();
 
+// This 'if' statement prevents application log messages from
+// displaying in the stdout when the tests are run
+if (process.env.NODE_ENV !== 'test') {
+  app.use(logger('dev'));
+}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use('/api/v1/', routes)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -15,6 +23,27 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+if(app.get('env') === 'development' || app.get('env') === 'test') {
+  app.use((err,req,res,next) => {
+    console.log("error", err)
+    res.status(err.status || 500)
+    res.json({
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+//default to production
+app.use((err,req,res,next) => {
+    console.log("error", err)
+    res.status(err.status || 500)
+    res.json({
+      message: err.message,
+      error: {}
+    })
+  })
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
